e7dd97f91d2a876b720686511a3c40e51b9a4c43
Eyas5 <eyas.tah@gmail.com>
2016-05-25 17:07:31 +0300

    new theme2016

---
+++ web/app/themes/Theme2016/front-page.php
@@ -0,0 +1,64 @@
+<?php /* Template Name: Front Page */ ?>
+
+<?php get_header(); ?>
+<html>
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+<div class="one">
+
+
+
+
+
+
+<div class="two">
+    <div class="logo">
+        <a href="https://integrify.fi/"><img class="logo2" src="<?php echo get_template_directory_uri() ?>/assets/images/integrify-logo-neg.svg" alt="logo pic" style="width:150px;height:60.5px;">
+        </a> </div>
+    <img class="dude" src="<?php echo get_template_directory_uri() ?>/assets/images/main-header-bg.jpg" alt="dude pic" style="width:1153px;height:450px;" >
+
+    <h1 class="heading">Enabling integration through technology.</h1>
+    <h2 class="sub-heading">Join integrify's sponsored career program to become a world-class coder now!</h2>
+
+</div>
+
+
+
+
+
+    <div class="three">
+        <img class="section" src="<?php echo get_template_directory_uri() ?>/assets/images/section-1-bg.jpg" alt="section1" style="width:580px;height:433px;" >
+
+        <h2 class="heading3">What is Integrify?</h2>
+        <p>Integrify is a software development center for refugees, asylum seekers, and recent immigrants. We want to help you kick-start your new life in Finland by providing you with world-class education, equipment, and job opportunities.</p>
+        <p>Integrify is a refugee camp reinvented. Instead of operating as a housing facility with basic services, Integrify takes a results-oriented approach by integrating its residents into society through technology and jobs. To accomplish this, Integrify focuses on the ultimate meritocracy; software development.</p>
+        <p>Questions? Contact CEO Daniel Rahman, <a href="tel:+358505650448">+358 50 5650 448</a>, <a href="mailto:daniel@integrify.fi">daniel@integrify.fi</a></p>
+
+    </div>
+
+
+    <div class="four">
+
+        <h2 class="heading4">What’s in it for me?</h2>
+
+                <p>Today, it takes on average multiple years for an asylum seeker to get a low-skill job in Finland. Integrify empowers refugees to take part in the start-up eco system and fill developer positions after only 6-12 months. The first results are in; within the first weeks of operation Integrify's pilot developer center has already placed two refugees into employment in downtown Helsinki.</p>
+                <p>Questions? Check out our <a ui-sref="faq" href=" http://integrify.dev/faq/">FAQ</a></p>
+
+
+
+
+
+    </div>
+
+
+
+</div>
+
+</body>
+
+</html>
+
+<?php get_footer(); ?>
---
+++ web/app/themes/Theme2016/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> </title>
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>> 
